6ed8489f1204499512fa66ed8770d1a311a02550
Sidorchenko Olga <olya.sidorchenko@gmail.com>
2017-04-24 17:58:43 +0300

    client_editor_activity

---
+++ app/src/main/java/com/example/sidorchenkoolga/max_decor/client_editor.java
@@ -0,0 +1,13 @@
+package com.example.sidorchenkoolga.max_decor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class client_editor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_editor);
+    }
+}
