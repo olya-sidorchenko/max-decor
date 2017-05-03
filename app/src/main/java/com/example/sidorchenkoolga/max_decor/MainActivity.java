c4aa9eb0f22ea861084746ce64485d7711dd1540
Sidorchenko Olga <olya.sidorchenko@gmail.com>
2017-05-03 18:57:19 +0300

    Обработчики кнопок в MainActivity. Переключение на UserSettingActivity и client_Editor

--- app/src/main/java/com/example/sidorchenkoolga/max_decor/MainActivity.java
+++ app/src/main/java/com/example/sidorchenkoolga/max_decor/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.sidorchenkoolga.max_decor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button userSettingsButton = (Button)findViewById(R.id.userSettingsButton);
+        userSettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ActUser = new Intent(getApplicationContext(), UserSettingsActivity.class);
+                startActivity(ActUser);
+            }
+        });
+
+        Button clientEditorButton = (Button)findViewById(R.id.clientEditorButton);
+        clientEditorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ClientEd = new Intent(getApplicationContext(), client_editor.class);
+                startActivity(ClientEd);
+            }
+        });
     }
 }
